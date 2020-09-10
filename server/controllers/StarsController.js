6e4ed4acd6c8f723819ff87ba2b2bb471469e2b6
Ben W <bwiedeback@gmail.com>
2020-09-10 10:05:04 -0600

    created message

--- server/controllers/StarsController.js
+++ server/controllers/StarsController.js
@@ -31,7 +31,7 @@ export class StarsController extends BaseController {
   async create(req, res, next) {
     try {
       let data = await starsService.create(req.body);
-      res.send(data)
+      res.status(201).send(data)
     } catch (error) {
       next(error)
     }
